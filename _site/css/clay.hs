98f7a8e837e5390f8450e1bdc4d529dc4f07fa13
Tobi Oloke <93312805+tobz619@users.noreply.github.com>
2022-07-03 15:56:18 +0100

    Mysite030722

---
+++ _site/css/clay.hs
@@ -0,0 +1,1 @@
+module MyCss where{-# LANGUAGE OverloadedStrings #-}import Clay import qualified Data.Text.Lazy.IO as T test ::Css test = undefined fancyButton ::Css fancyButton = ".fancy-button" ? do backgroundColor tomato sym2 padding (em 0.5) (em 1) sym borderRadius (px 5) color white border solid (px 1) red hover & do color green main ::IO () main - T.putStr $ renderWith compact test
---
+++ contact.markdown
@@ -0,0 +1,11 @@
+---
+title: Contact and Socials!
+---
+
+Email: <a href = "mailto: toloke@yahoo.co.uk">toloke@yahoo.co.uk</a>
+
+Twitter: <a href = "https://twitter.com/tobz619" > tobz619 </a>
+
+YouTube: <a href = "https://www.youtube.com/channel/UCjA_GLW3WznqqkdrVOnA3FA"> Check it! </a>
+
+Instagram: <a href = "https://www.instagram.com/tobioloke_/"> @tobioloke_ </a>
