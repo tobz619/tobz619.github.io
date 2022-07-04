5c2090ff2a4b68a7a708df37df05beb5c8e2fd40
tobz619 <93312805+tobz619@users.noreply.github.com>
2022-07-04 13:02:39 +0100

    Change YouTube & config

--- site.hs
+++ site.hs
@@ -9,7 +9,7 @@ config :: Configuration
 config = defaultConfiguration {destinationDirectory = "docs"}
 
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
